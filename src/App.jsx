0e00a83038d0295c4e1ef2dbe099783cf1197ddf
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-05-29 12:29:15 -0700

    start in project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="min-h-screen flex items-center justify-center bg-red-100"></div>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // مهم جدًا عشان Tailwind يشتغل على ملفات React
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
